2cfc7acf31a8dfe5b50a9563ac87c24715d3aec8
psq <psq@wumis.com>
2022-02-27 12:30:47 +0800

    day12:矩阵中的路径-使用深度优先搜索(DFS) 周赛:完成旅途的最少时间-使用二分查找

--- src/main/java/com/psq/learn/offer/MediumCode.java
+++ src/main/java/com/psq/learn/offer/MediumCode.java
@@ -134,4 +134,108 @@ public class MediumCode {
         }
 
     }
+
+    /**
+     *  矩阵中的路径
+     *  题目：给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+     * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用
+     */
+    class day12{
+        class Solution {
+            /**
+             *TODO：方法：深度优先搜索(DFS) + 剪枝
+             * 解题思路：
+             * 深度优先搜索： 可以理解为暴力法遍历矩阵中所有字符串可能性。DFS 通过递归，先朝一个方向搜到底，再回溯至上个节点，沿另一个方向搜索，以此类推。
+             * 剪枝： 在搜索中，遇到 这条路不可能和目标字符串匹配成功 的情况（例如：此矩阵元素和目标字符不同、此元素已被访问），则应立即返回，称之为 可行性剪枝 。
+             *  DFS 解析：
+             * 递归参数： 当前元素在矩阵 board 中的行列索引 i 和 j ，当前目标字符在 word 中的索引 k 。
+             * 终止条件：
+             * 返回 false ： (1) 行或列索引越界 或 (2) 当前矩阵元素与目标字符不同 或 (3) 当前矩阵元素已访问过 （(3)可合并至 (2)，将访问的值重新赋值） 。
+             * 返回 true ： k = len(word) - 1 ，即字符串 word 已全部匹配。
+             * 递推工作：
+             * 标记当前矩阵元素： 将 board[i][j] 修改为 空字符 '' ，代表此元素已访问过，防止之后搜索时重复访问。
+             * 搜索下一单元格： 朝当前元素的 上、下、左、右 四个方向开启下层递归，使用 或 连接 （代表只需找到一条可行路径就直接返回，不再做后续 DFS ），并记录结果至 res 。
+             * 还原当前矩阵元素： 将 board[i][j] 元素还原至初始值，即 word[k] 。
+             * 返回值： 返回布尔量 res ，代表是否搜索到目标字符串
+             *
+             * 复杂度分析：
+             * M, N分别为矩阵行列大小， K 为字符串 word 长度。
+             * 时间复杂度 O(3^KMN) ： 最差情况下，需要遍历矩阵中长度为 K 字符串的所有方案，时间复杂度为 O(3^K)；矩阵中共有 MN 个起点，时间复杂度为 O(MN) 。
+             *  方案数计算： 设字符串长度为 K ，搜索中每个字符有上、下、左、右四个方向可以选择，舍弃回头（上个字符）的方向，剩下 3 种选择，因此方案数的复杂度为 O(3^K)
+             *
+             * 空间复杂度 O(K) ： 搜索过程中的递归深度不超过 K ，因此系统因函数调用累计使用的栈空间占用 O(K)（因为函数返回后，系统调用的栈空间会释放）。最坏情况下 K = MNK ，递归深度为 MNMN ，此时系统栈使用 O(MN)的额外空间。
+             */
+            public boolean exist(char[][] board, String word) {
+                char[] words = word.toCharArray();
+                for(int i=0; i<  board.length; i++){
+                    for(int j=0; j<board[0].length; j++){
+                        if(dfs(board, words, i, j, 0)){
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            //广度优先搜索
+            public boolean dfs(char[][] board, char[] words, int i, int j, int k){
+                //越界及当前单词不匹配都返回fasle
+                if(i<0 || i >= board.length || j<0 || j >= board[0].length || board[i][j] != words[k]){
+                    return false;
+                }
+                //代表words字符已全部匹配
+                if(k == (words.length - 1)){
+                    return true;
+                }
+                //先赋值，代表已被搜索过,下面进行判断即可区分跳过
+                board[i][j] = '\0';
+
+                //分别对上下左右进行判断
+                boolean result = (dfs(board, words, i+1, j, k+1) || dfs(board, words, i-1, j, k+1)
+                        ||dfs(board, words, i, j+1, k+1) || dfs(board, words, i, j-1 ,k+1));
+                //再还原矩阵的值
+                board[i][j] = words[k];
+                return result;
+            }
+        }
+    }
+
+    /**
+     * TODO：周赛-6010
+     * 完成旅途的最少时间 显示英文描述
+     * 给你一个数组 time ，其中 time[i] 表示第 i 辆公交车完成 一趟旅途 所需要花费的时间。
+     * 每辆公交车可以 连续 完成多趟旅途，也就是说，一辆公交车当前旅途完成后，可以 立马开始 下一趟旅途。每辆公交车 独立 运行，也就是说可以同时有多辆公交车在运行且互不影响。
+     * 给你一个整数 totalTrips ，表示所有公交车 总共 需要完成的旅途数目。请你返回完成 至少 totalTrips 趟旅途需要花费的 最少 时间。
+     */
+    class Week_6010 {
+        /**
+         * 方法：二分查找
+         *
+         */
+        public long minimumTime(int[] time, int totalTrips) {
+            long r=0;
+            long max = 0;
+            for(int a: time){
+                max = Math.max(max,a);
+            }
+            r= max*totalTrips;
+            long l = 1;
+            while(l<r){
+                long mid = (l+r)/2;
+                if(check(time, mid, totalTrips)){
+                    r = mid;
+                }else{
+                    l = mid +1;
+                }
+            }
+            return l;
+        }
+        public boolean check(int[] time, long mid, int totalTrips){
+            long count = 0;
+            for(int a : time){
+                count += mid/a;
+            }
+            return count>=totalTrips;
+        }
+    }
 }
