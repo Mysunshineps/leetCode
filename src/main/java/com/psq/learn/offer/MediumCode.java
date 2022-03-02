4d711b71a6614a6ef883125aefaa613da347dda7
psq <psq@wumis.com>
2022-03-02 20:53:48 +0800

    day15:剪绳子II 与day14思路一致，但要考虑越界问题

--- src/main/java/com/psq/learn/offer/MediumCode.java
+++ src/main/java/com/psq/learn/offer/MediumCode.java
@@ -307,6 +307,7 @@ public class MediumCode {
     /**
      *  剪绳子
      *  题目：给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+     *  提示：2 <= n <= 58
      */
     class day14{
         /**
@@ -348,6 +349,40 @@ public class MediumCode {
                 return 2*2*(int)Math.pow(3, a-1);
             }
             return (int)Math.pow(3, a);
+        }
+    }
+
+    /**
+     * 剪绳子 II
+     * 题目：给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m - 1] 。请问 k[0]*k[1]*...*k[m - 1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+     * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     * 提示：2 <= n <= 1000；会存在int越界
+     */
+    class day15{
+        /**
+         * 与day14解题思路一致；
+         * TODO：但要考虑越界的问题
+         * 具体看：https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof/solution/mian-shi-ti-14-ii-jian-sheng-zi-iitan-xin-er-fen-f/
+         */
+        public int cuttingRope(int n) {
+            if(n <= 3) {
+                return n - 1;
+            }
+            int b = n % 3, p = 1000000007;
+            long rem = 1, x = 3;
+            for(int a = n / 3 - 1; a > 0; a /= 2) {
+                if(a % 2 == 1) {
+                    rem = (rem * x) % p;
+                }
+                x = (x * x) % p;
+            }
+            if(b == 0)  {
+                return (int)(rem * 3 % p);
+            }
+            if(b == 1) {
+                return (int)(rem * 4 % p);
+            }
+            return (int)(rem * 6 % p);
         }
     }
 
