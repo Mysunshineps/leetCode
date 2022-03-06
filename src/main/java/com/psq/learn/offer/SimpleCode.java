1fdbd4d0504370906dcc6a17253b05790c69b024
psq <psq@wumis.com>
2022-03-06 21:27:47 +0800

    day19:Excel 表中某个范围内的单元格 数学推导

--- src/main/java/com/psq/learn/offer/SimpleCode.java
+++ src/main/java/com/psq/learn/offer/SimpleCode.java
@@ -1,7 +1,9 @@
 package com.psq.learn.offer;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @Description 简单程度
@@ -257,6 +259,41 @@ public class SimpleCode {
                 res[i] = i + 1;
             }
             return res;
+        }
+    }
+
+    /**
+     * Excel 表中某个范围内的单元格
+     * 题目：Excel 表中的一个单元格 (r, c) 会以字符串 "<col><row>" 的形式进行表示，其中：
+     *
+     * <col> 即单元格的列号 c 。用英文字母表中的 字母 标识。
+     * 例如，第 1 列用 'A' 表示，第 2 列用 'B' 表示，第 3 列用 'C' 表示，以此类推。
+     * <row> 即单元格的行号 r 。第 r 行就用 整数 r 标识。
+     * 给你一个格式为 "<col1><row1>:<col2><row2>" 的字符串 s ，其中 <col1> 表示 c1 列，<row1> 表示 r1 行，<col2> 表示 c2 列，<row2> 表示 r2 行，并满足 r1 <= r2 且 c1 <= c2 。
+     *
+     * 找出所有满足 r1 <= x <= r2 且 c1 <= y <= c2 的单元格，并以列表形式返回。单元格应该按前面描述的格式用 字符串 表示，并以 非递减 顺序排列（先按列排，再按行排）
+     */
+    class day19{
+        /**
+         * 数学推导即可
+         */
+        public List<String> cellsInRange(String s) {
+            ArrayList<String> result = new ArrayList<>();
+            String[] array = s.split(":");
+            char[] a = array[0].toCharArray();
+            char[] b = array[1].toCharArray();
+            int c1 = (int)a[0];
+            int c2 = (int)b[0];
+            //字母转数字：减去字符'0'
+            int r1 = a[1] - '0';
+            int r2 = b[1] - '0';
+            for(int i=c1; i<=c2; i++){
+                String col = String.valueOf((char)(i));
+                for(int j=r1; j<=r2; j++){
+                    result.add(col + j);
+                }
+            }
+            return result;
         }
     }
 }
