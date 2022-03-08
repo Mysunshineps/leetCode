510458a971e47537af5fc617253d0f1bd10ca783
psq <psq@wumis.com>
2022-03-08 19:57:50 +0800

    day21:调整数组顺序使奇数位于偶数前面 双指针

--- src/main/java/com/psq/learn/offer/SimpleCode.java
+++ src/main/java/com/psq/learn/offer/SimpleCode.java
@@ -337,4 +337,40 @@ public class SimpleCode {
             return head;
         }
     }
+
+    /**
+     * 调整数组顺序使奇数位于偶数前面
+     * 题目：输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数在数组的前半部分，所有偶数在数组的后半部分。
+     */
+    class day21{
+        /**
+         * 解题思路：
+         * 算法流程：
+         * 初始化： i , j 双指针，分别指向数组 nums 左右两端；
+         * 循环交换： 当 i = j时跳出；
+         * 指针 i 遇到奇数则执行 i = i + 1跳过，直到找到偶数；
+         * 指针 j 遇到偶数则执行 j = j - 1 跳过，直到找到奇数；
+         * 交换 nums[i] 和 nums[j] 值；
+         * 返回值： 返回已修改的 nums 数组
+         *
+         *复杂度分析：
+         * 时间复杂度 O(N) ： N 为数组 nums 长度，双指针 i, j 共同遍历整个数组。
+         * 空间复杂度 O(1) ： 双指针 i, j 使用常数大小的额外空间
+         */
+        public int[] exchange(int[] nums) {
+            int i = 0, j = nums.length - 1, tmp;
+            while(i < j) {
+                while(i < j && (nums[i] & 1) == 1) {
+                    i++;
+                }
+                while(i < j && (nums[j] & 1) == 0) {
+                    j--;
+                }
+                tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+            return nums;
+        }
+    }
 }
