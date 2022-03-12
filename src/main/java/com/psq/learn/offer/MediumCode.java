ae27e037876ad36bc5148aded763e6c73e92cff5
psq <psq@wumis.com>
2022-03-12 22:04:24 +0800

    day25:树的子结构 先序遍历+递归+思维

--- src/main/java/com/psq/learn/offer/MediumCode.java
+++ src/main/java/com/psq/learn/offer/MediumCode.java
@@ -413,4 +413,54 @@ public class MediumCode {
             return res;
         }
     }
+
+    /**
+     * 树的子结构
+     * 题目：输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)；B是A的子结构， 即 A中有出现和B相同的结构和节点值
+     */
+    class day25{
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode(int x) { val = x; }
+        }
+
+        /**
+         *
+         *算法流程：
+         * 名词规定：树 A 的根节点记作 节点 A ，树 B 的根节点称为 节点 B 。
+         * 1.recur(A, B) 函数-终止条件：
+         *  当节点 B 为空：说明树 B 已匹配完成（越过叶子节点），因此返回 true ；
+         *  当节点 A 为空：说明已经越过树 A叶子节点，即匹配失败，返回 false ；
+         *  当节点 A 和 B 的值不同：说明匹配失败，返回 false ；
+         *  返回值：
+         *  判断 AA 和 B 的左子节点是否相等，即 recur(A.left, B.left) ；
+         *  判断 A 和 B 的右子节点是否相等，即 recur(A.right, B.right) ；
+         * 2.isSubTree(A, B) 函数：
+         * 特例处理： 当 树 A 为空 或 树 B 为空 时，直接返回 false ；
+         * 返回值： 若树 B 是树 A 的子结构，则必满足以下三种情况之一，因此用或 || 连接；
+         *  以 节点 A 为根节点的子树 包含树 B ，对应 recur(A, B)；
+         *  树 B 是 树 A 左子树 的子结构，对应 isSubStructure(A.left, B)；
+         *  树 B 是 树 A 右子树 的子结构，对应 isSubStructure(A.right, B)；
+         *
+         * 复杂度分析：
+         *  时间复杂度 O(MN) ： 其中 M,N分别为树 A 和 树 B 的节点数量；先序遍历树 A 占用 O(M) ，每次调用 recur(A, B) 判断占用 O(N) 。
+         *  空间复杂度 O(M) ： 当树 A 和树 B 都退化为链表时，递归调用深度最大。当 M≤N 时，遍历树 A 与递归判断的总递归深度为 M ；当 M>N 时，最差情况为遍历至树 A 叶子节点，此时总递归深度为 M。
+         */
+        public boolean isSubStructure(TreeNode A, TreeNode B) {
+            return  (null!=A && null!=B) && (isSubTree(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+
+        }
+
+        private boolean isSubTree(TreeNode ta, TreeNode tb){
+            if(tb == null){
+                return true;
+            }
+            if(ta == null || ta.val != tb.val){
+                return false;
+            }
+            return (isSubTree(ta.left, tb.left) && isSubTree(ta.right, tb.right));
+        }
+    }
 }
