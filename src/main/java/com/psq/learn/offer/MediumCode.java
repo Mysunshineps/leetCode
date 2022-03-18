ce2d1cf37b910fb5e40c3ffc9bfb1c25229c8784
psq <psq@wumis.com>
2022-03-18 20:48:42 +0800

    day31:栈的压入、弹出序列 辅助栈

--- src/main/java/com/psq/learn/offer/MediumCode.java
+++ src/main/java/com/psq/learn/offer/MediumCode.java
@@ -3,6 +3,7 @@ package com.psq.learn.offer;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @Description 中等程度
@@ -461,6 +462,31 @@ public class MediumCode {
                 return false;
             }
             return (isSubTree(ta.left, tb.left) && isSubTree(ta.right, tb.right));
+        }
+    }
+
+    /**
+     * 栈的压入、弹出序列
+     * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如，序列 {1,2,3,4,5} 是某栈的压栈序列，序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列
+     */
+    class day31 {
+        /**
+         * 辅助栈，有待优化
+         */
+        public boolean validateStackSequences(int[] pushed, int[] popped) {
+            int k = 0;
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < pushed.length; i++) {
+                stack.push(pushed[i]);
+                while (!stack.empty() && stack.peek().equals(popped[k])) {
+                    stack.pop();
+                    k++;
+                }
+            }
+            if (stack.empty()) {
+                return true;
+            }
+            return false;
         }
     }
 }
